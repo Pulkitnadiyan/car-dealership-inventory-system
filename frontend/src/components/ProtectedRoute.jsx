90a35c89ca7e94b069a296554352435c80413a05
Pulkitnadiyan <nadiyanpulkit06@gmail.com>
2026-07-30 11:02:39 +0530

    feat: implement protected route

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,1 +1,15 @@
-export default function ProtectedRoute() { }
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+function ProtectedRoute({ children }) {
+
+    const { token } = useAuth();
+
+    if (!token) {
+        return <Navigate to="/login" replace />;
+    }
+
+    return children;
+}
+
+export default ProtectedRoute;
